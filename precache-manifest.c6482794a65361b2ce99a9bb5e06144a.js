8322f49c32467ce25aaafa9f4562aeea10b48f0c
Max <maksymilian.dabrowski93@gmail.com>
2019-04-08 21:04:48 +0200

    Updates

---
+++ precache-manifest.c6482794a65361b2ce99a9bb5e06144a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff3c927c5d8ff458bb07",
+    "url": "/portfolio/static/css/main.2f2d13c1.chunk.css"
+  },
+  {
+    "revision": "ff3c927c5d8ff458bb07",
+    "url": "/portfolio/static/js/main.ff3c927c.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "809dba9926cbd07e2a71",
+    "url": "/portfolio/static/js/2.809dba99.chunk.js"
+  },
+  {
+    "revision": "229af50d563ec8f1b5ede25c09dd1694",
+    "url": "/portfolio/static/media/img1.229af50d.jpg"
+  },
+  {
+    "revision": "05cf33cfebb8b4a555f832136f6eb747",
+    "url": "/portfolio/static/media/img2.05cf33cf.jpg"
+  },
+  {
+    "revision": "343beaff1ad1031c8a07214fd80becea",
+    "url": "/portfolio/static/media/img3.343beaff.jpg"
+  },
+  {
+    "revision": "136abc1d10c6a82886cf03ce7b094778",
+    "url": "/portfolio/static/media/img4.136abc1d.jpg"
+  },
+  {
+    "revision": "92875ee7cd9e563843f0075063416ebd",
+    "url": "/portfolio/static/media/img5.92875ee7.jpg"
+  },
+  {
+    "revision": "2f90edf7f92b1f5ae8791396971293db",
+    "url": "/portfolio/static/media/CV.2f90edf7.pdf"
+  },
+  {
+    "revision": "691ed556f927359b31565b41881d9a49",
+    "url": "/portfolio/static/media/max.691ed556.jpg"
+  },
+  {
+    "revision": "ce826ebb787dc604b90da58c3fc34a0f",
+    "url": "/portfolio/static/media/ToDoApp.ce826ebb.JPG"
+  },
+  {
+    "revision": "e1d37d745c38df33edd406a04296ab3f",
+    "url": "/portfolio/static/media/bussinesCard.e1d37d74.JPG"
+  },
+  {
+    "revision": "7695c234a063d54ffd50b4e5097df173",
+    "url": "/portfolio/static/media/formValidation.7695c234.JPG"
+  },
+  {
+    "revision": "45363701dc63ce7d85e54983582bab19",
+    "url": "/portfolio/static/media/memoryGame.45363701.JPG"
+  },
+  {
+    "revision": "bebbac2c065bf31d668884c049b87325",
+    "url": "/portfolio/static/media/shipBattle.bebbac2c.JPG"
+  },
+  {
+    "revision": "f4ad03d72ff7aeb8c26477ee561d14d8",
+    "url": "/portfolio/static/media/smallWebShop.f4ad03d7.JPG"
+  },
+  {
+    "revision": "d76781e40e7342113a67e5a2eba654af",
+    "url": "/portfolio/static/media/weatherApp.d76781e4.JPG"
+  },
+  {
+    "revision": "9424b076d9612e501d2f7ea9285a4af6",
+    "url": "/portfolio/static/media/webDiary.9424b076.JPG"
+  },
+  {
+    "revision": "c2369ab044c956c10c168c0839de560d",
+    "url": "/portfolio/index.html"
+  }
+];
